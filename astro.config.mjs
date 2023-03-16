923dc42344a93b9291257a8379a40116fe0c464b
Letochová Nela <letochova@NLETOCHOVAN.oksystem.local>
2023-03-16 11:59:40 +0100

    Create project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astro-blog-cip.netlify.app",
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap({
+    filter: page => page !== "https://astro-blog-cip.netlify.app/about/"
+  }), react(), tailwind(), mdx()]
+});
---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button
+      className="bg-purple-600"
+      onClick={() => {
+        setCount((prev) => prev + 1);
+      }}
+    >
+      {count}
+    </button>
+  );
+};
+
+export default Counter;
